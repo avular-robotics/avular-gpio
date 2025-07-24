6052b171defeef7dd98d2e1817c856dfcfe4c3c9
Robbert-Jan de Jager <rj.jager@avular.com>
2025-07-24 12:58:55 +0200

    feat: Add support for the origin

--- lib/python/Avular/GPIO/gpio_pin_data.py
+++ lib/python/Avular/GPIO/gpio_pin_data.py
@@ -163,6 +163,7 @@ compats_jetson_orins_nx = (
     "nvidia,p3768-0000+p3767-0001",
     "nvidia,p3768-0000+p3767-0000-super",
     "nvidia,p3768-0000+p3767-0001-super",
+    "avular,origin-r05",
 )
 
 compats_jetson_orins_nano = (
